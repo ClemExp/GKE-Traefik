d3b964f1cb9ea1c3f771d211515beaf576055956
Clem O Regan <clemoregan@gmail.com>
2023-08-18 19:45:14 +0100

    Enhance monitoring

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,10 @@ terraform {
     helm = {
       source = "hashicorp/helm"
     }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.11.0"
+    }
   }
 }
 
@@ -15,6 +19,15 @@ provider "google" {
   project = var.project
   region = var.region
   zone = var.zone
+}
+
+provider "kubectl" {
+    load_config_file       = false
+    host = google_container_cluster.primary.endpoint
+    token = data.google_client_config.current.access_token
+    client_certificate = base64decode(google_container_cluster.primary.master_auth.0.client_certificate)
+    client_key = base64decode(google_container_cluster.primary.master_auth.0.client_key)
+    cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
 }
 
 resource "google_container_cluster" "primary" {
---
+++ terraform/monitoring_ingress.tf
@@ -0,0 +1,11 @@
+
+# Following ingress routes are to publish access to monitoring tools through traefik
+resource "kubectl_manifest" "grafana_ingress_route" {
+  yaml_body = "${file("../ingress_access/grafana.yaml")}"
+  depends_on = [null_resource.monitoring_stack]
+}
+
+resource "kubectl_manifest" "prometheus_ingress_route" {
+  yaml_body = "${file("../ingress_access/prom.yaml")}"
+  depends_on = [null_resource.monitoring_stack]
+}
