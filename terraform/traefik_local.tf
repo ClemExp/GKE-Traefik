e9e495718a73cdbec644b12185b1e78eb96435e1
Clem O Regan <clemoregan@gmail.com>
2023-09-02 23:02:52 +0200

    terraform update of helm for traefik

--- terraform/traefik_local.tf
+++ terraform/traefik_local.tf
@@ -4,7 +4,7 @@ resource "null_resource" "traefik_deploy" {
   depends_on = [google_compute_backend_service.backend_service]
 
   provisioner "local-exec" {
-    command = "helm install traefik ../helm/traefik-dashboard --values ../helm/traefik-dashboard/values.yaml --set varsubdomain='clemoregan.com' -n tlson --create-namespace"
+    command = "helm upgrade --install traefik ../helm/traefik-dashboard --values ../helm/traefik-dashboard/values.yaml --set varsubdomain='clemoregan.com' -n tlson --create-namespace"
     interpreter = local.is_windows ? ["PowerShell", "-Command"] : []
   }
 }
